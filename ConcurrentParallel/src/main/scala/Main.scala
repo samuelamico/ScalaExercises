6dcc82e83d1cc0cc9929d0e27649093aceb35193
samuelamico <sam.fst@gmail.com>
2020-06-15 19:55:57 -0300

    Threads low-level class

--- ConcurrentParallel/src/main/scala/Main.scala
+++ ConcurrentParallel/src/main/scala/Main.scala
@@ -11,3 +11,45 @@ object Main extends App{
   println(Functions.permutations("Chuchu"))
 
 }
+
+object ThreadMain extends App{
+
+  def thread(body: => Unit): Thread = {
+      val t = new Thread {
+        override def run() = body
+      }
+      t.start()
+      t
+  }
+
+  class MyThread extends Thread {
+    override def run(): Unit = {
+        Thread.sleep(1000)
+        println("Ola Thread")
+        Thread.sleep(1000)
+
+    }
+  }
+
+  // Using this.synchronized eliminates race conditions
+  var uidCount = 0L
+  def getUniqueId() = this.synchronized {
+    val freshUid = uidCount + 1
+    uidCount = freshUid
+    freshUid
+  }
+
+  def printUniqueIds(n: Int): Unit = {
+    val uids = for (i<- 0 until n) yield getUniqueId()
+    println(s"Generated uids: $uids")
+  }
+
+
+
+  //val thread1 = new MyThread
+  val t1 = thread({printUniqueIds(5)})
+  printUniqueIds(5)
+
+  println("New thread Joined")
+
+}
